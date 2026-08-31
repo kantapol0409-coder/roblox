cf55a6c2e8e39f843a6681c8e8b86e4fd7870070
x <chatchai09122546@gmail.com>
2026-08-31 15:23:20 +0700

    Add official loader.lua and main.lua

---
+++ main.lua
@@ -0,0 +1,6 @@
+-- ==============================================================================
+-- ⚡ STEAL AN EGG & PET - OFFICIAL LOADER
+-- 👑 GitHub: https://github.com/kantapol0409-coder/roblox
+-- ==============================================================================
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kantapol0409-coder/roblox/main/auto_pilot_v10.lua"))()
